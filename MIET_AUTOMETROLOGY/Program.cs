cbdcae505137e5a9ca1c1a6622b16843cc8cb5bb
Stas_Stepanov <st.stanislove@yandex.ru>
2023-12-03 22:47:49 +0300

    Второй коммит

--- MIET_AUTOMETROLOGY/Program.cs
+++ MIET_AUTOMETROLOGY/Program.cs
@@ -2,6 +2,25 @@
 {
     static void Main()
     {
-        Console.WriteLine("Привет, мир!");
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+
+        }
+
+        // Создать строку
+        string writeText = "Привет, мир!";
+
+        // Создать файл и записать в него содержимое строки
+        File.WriteAllText("TextFile1.txt", writeText);
+
+        // Прочитать содержимое файла в строку
+        string readText = File.ReadAllText("TextFile1.txt");
+
+        // Вывести содержимое на консоль
+        Console.WriteLine(readText);
+
+        // Добавить строку к файлу
+        File.AppendAllText("TextFile1.txt", "\nЭто добавленная строка.");
     }
 }
